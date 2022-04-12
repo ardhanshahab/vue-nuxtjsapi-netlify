d7e11d0d563ba119dfa19f845dad170255c81cc5
ardhanshahab <ardhanshahab@gmail.com>
2022-04-12 13:35:00 +0700

    feat : tugas PWA

--- pages/index.vue
+++ pages/index.vue
@@ -1,87 +1,50 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          src="https://www.iconpacks.net/icons/1/free-movie-icon-850-thumb.png"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to Movie International ID
+        </h1>
+
+        <p class="subheading font-weight-regular">
+          Klik tombol dibawah untuk ke halaman Category
+          <br/>
+          <br/>
+          <br/>
+          
+          <v-btn
+          light
+          color="blue lighten-4"
+          href="/movie"
+          >GO!</v-btn>
+        </p>
+      </v-col>
+
+      
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'IndexPage',
-}
+  export default {
+    name: 'HelloWorld',
+
+    data() {
+      
+    },
+    methods: {
+        redirectnews() {
+        this.$router.push('/movie');
+    }
+    }
+  }
 </script>
